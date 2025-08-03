994d117a05649eceb3b70dbb88bc2dd029b14593
dscreet <discreetbinger@gmail.com>
2025-08-03 16:29:39 +0100

    verified sqlite integration with prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// typed reference to global object
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// ensures reusing client if exists
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+// prevents creating multiple connections during hot reload in dev
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
