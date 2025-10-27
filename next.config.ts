7c86aa46b4f8615eef0ee754e78a527be91db213
dscreet <discreetbinger@gmail.com>
2025-10-27 21:50:31 +0000

    inital list of movies ui done

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [new URL('https://image.tmdb.org/t/p/**')],
+  },
   /* config options here */
 };
 
