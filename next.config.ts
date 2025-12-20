f1beffe72d13d8785f35d473f85d4d411b8642c1
dscreet <discreetbinger@gmail.com>
2025-12-20 21:00:32 +0000

    unoptimise images to see changes in vercel usage

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://image.tmdb.org/t/p/**')],
-    unoptimized: process.env.CI === 'true', // to prevent tiemout issues in ci
+    // unoptimized: process.env.CI === 'true', // to prevent timeout issues in ci
+    unoptimized: true,
   },
   /* config options here */
 };
