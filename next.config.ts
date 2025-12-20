aa7c39345dcb5400a570dd769b4a5ae94fd0a030
dscreet <discreetbinger@gmail.com>
2025-12-20 00:31:34 +0000

    nextjs images unootimised for ci for speed

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://image.tmdb.org/t/p/**')],
+    unoptimized: process.env.CI === 'true', // to prevent tiemout issues in ci
   },
   /* config options here */
 };
