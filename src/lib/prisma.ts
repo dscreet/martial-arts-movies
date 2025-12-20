1154de985ca7975867a5e1c035a35f6ce31b615a
dscreet <discreetbinger@gmail.com>
2025-12-20 16:45:10 +0000

    attempt to fix sqlite not being found in prod

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,22 @@
 import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
 // typed reference to global object
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const filePath = path.join(process.cwd(), 'prisma/dev.db');
+const config = {
+  datasources: {
+    db: {
+      url: 'file:' + filePath,
+    },
+  },
+};
+
 // ensures reusing client if exists
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient(config);
 
 // prevents creating multiple connections during hot reload in dev
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
