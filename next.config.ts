8feda546f2d4e1b8261a49a4b4a8997f653ddb34
dscreet <discreetbinger@gmail.com>
2025-12-21 18:14:48 +0000

    for testing image optimisaiton on differnet host platforms

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://image.tmdb.org/t/p/**')],
-    // unoptimized: process.env.CI === 'true', // to prevent timeout issues in ci
-    unoptimized: true,
+    unoptimized: process.env.CI === 'true', // to prevent timeout issues in ci
+    // unoptimized: true,
   },
   /* config options here */
 };
